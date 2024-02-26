357d2e5ea08010e5b248e883c4b52692be421a3a
praveenreddysheelam <praveenreddysheelam199719@gmail.com>
2024-02-26 17:41:33 -0500

    filters

---
+++ src/components/BodyComponent.js
@@ -0,0 +1,50 @@
+import RestaurantCard from "./RestaurantCard";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+const BodyComponent =() => {
+    const [listOfRestaurants,setlistOfRestaurants]=useState([]);
+    const [filteredList,setfilteredList]=useState([]);
+    const [searchText,setsearchText]=useState("");
+    useEffect(()=>{
+        fetchData();
+    },[])
+    const fetchData = async () => {
+        const data= await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+        const json= await data.json();
+        setlistOfRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setfilteredList(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+    
+    const handleSearch =() =>{
+        console.log("search");
+        searchlist=listOfRestaurants.filter((res)=>res.info.name.toLowerCase().includes(searchText.toLowerCase()))
+        setfilteredList(searchlist);
+    }
+    console.log(searchText);
+    if(listOfRestaurants.length==0){
+        return(
+            <Shimmer />
+        );
+    }
+    return(
+        <div className="body">
+            <div className="filter">
+                <div className="search">
+                    <input type="text" className="search-box" value={searchText} onChange={(e)=>setsearchText(e.target.value)}></input>
+                    <button className="search-btn" onClick={handleSearch}>Search </button>
+                </div>
+                <button className="filter-btn" onClick={()=>{
+                   const filtered=listOfRestaurants.filter((res)=>res.info.avgRating>4.3);
+                   setfilteredList(filtered);
+                }} >Top Rated Restaurants </button>
+            </div>
+            <div className="res-container">
+               {
+                filteredList.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant} />)
+               }
+            </div>
+        </div>
+    );
+
+}
+export default BodyComponent;
